3ea558e619d497ad8d30917a8901d9b6122631f0
Nicholas Oliveira dos Santos <nicolasgoldtec@gmail.com>
2021-03-04 23:09:09 +0000

    Create Get e set

---
+++ src/main/java/br/com/impacta/models/Carro.java
@@ -0,0 +1,33 @@
+package br.com.impacta.models;
+
+public class Carro {
+  private String cor;
+  private int ano;
+  private double valor;
+  
+  
+  public void setValor(Double valor){
+    this.valor = valor;
+  }
+  
+  public double getValor(){
+    return this.valor;
+  }
+  
+  public void setCor(String cor){
+    this.cor = cor;
+  }
+  
+  public String getCor(){
+    return this.cor;
+  }
+  
+  public void setAno(int ano){
+    this.ano = ano;
+  }
+  
+  public int getAno(){
+    return this.ano;
+  }
+  
+}
